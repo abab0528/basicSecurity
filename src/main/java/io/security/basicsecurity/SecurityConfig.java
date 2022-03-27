c47bfbaa18dbe1c03f810954b386b860159ce15e
nahyunii <abab0528@naver.com>
2022-03-27 13:05:31 +0900

    add annotation

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -28,20 +28,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
         http
                 .formLogin()
-                .loginPage("/loginPage")
-                .defaultSuccessUrl("/")
-                .failureUrl("/loginPage")
-                .usernameParameter("userId")
-                .passwordParameter("passwd")
-                .loginProcessingUrl("/login_proc")
-                .successHandler(new AuthenticationSuccessHandler() {
+                .loginPage("/loginPage") //사용자 정의 로그인 페이지
+                .defaultSuccessUrl("/") //로그인 성공 후 이동 페이지
+                .failureUrl("/loginPage") //로그인 실패 후 이동 페이지
+                .usernameParameter("userId") //아이디 파라미터명 설정
+                .passwordParameter("passwd") //패스워드 파라미터명 설정
+                .loginProcessingUrl("/login_proc") // 로그인 Form action url
+                .successHandler(new AuthenticationSuccessHandler() { //로그인 성공 후 Handler
                     @Override
                     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
                         System.out.println("authentication : "+authentication.getName());
                         response.sendRedirect("/");
                     }
                 })
-                .failureHandler(new AuthenticationFailureHandler() {
+                .failureHandler(new AuthenticationFailureHandler() { //로그인 실패 후 Handler
                     @Override
                     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
                         System.out.println("exception : "+exception.getMessage());
@@ -51,23 +51,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll();
 
         http
-                .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login")
-                .addLogoutHandler(new LogoutHandler() {
+                .logout() //로그아웃 처리
+                .logoutUrl("/logout") //로그아웃 처리 URL
+                .logoutSuccessUrl("/login") //로그아웃 성공 후 이동 페이
+                .addLogoutHandler(new LogoutHandler() { //로그아웃 핸들러
                     @Override
                     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
                         HttpSession session = request.getSession();
                         session.invalidate();
                     }
                 })
-                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                .logoutSuccessHandler(new LogoutSuccessHandler() { //로그아웃 성공 후 핸들러
                     @Override
                     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
                         response.sendRedirect("/login");
                     }
                 })
-                .deleteCookies("remember-me")
+                .deleteCookies("remember-me") //로그아웃 후 쿠키 삭제
                 ;
     }
 }
